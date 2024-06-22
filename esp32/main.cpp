d94993d8eabb96bf2249e653534e4ef84702cbcb
roger <rch.goldsnaker@gmail.com>
2024-06-22 02:11:39 -0300

    Adding platformio files

--- esp32/main.cpp
+++ esp32/main.cpp
@@ -3,13 +3,27 @@
 #include <ThingsBoard.h>
 #include <ArduinoOTA.h>
 #include <LiquidCrystal_I2C.h>
+#include "DHT.h"
 
+// LCD
 #define I2C_ADDR    0x27
 #define LCD_COLUMNS 20
 #define LCD_LINES   4
 LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
 
-// Configuración de WiFi y ThingsBoard
+// DHT SENSOR
+#define DHTTYPE DHT22 
+#define DHT_PIN 15
+DHT dht(DHT_PIN, DHTTYPE);
+
+// LED Pins
+constexpr int LED_PIN1 = 25;
+constexpr int LED_PIN2 = 33;
+constexpr int LED_PIN3 = 32;
+constexpr int LED_PIN4 = 26;
+constexpr int LED_PIN5 = 27;
+
+// WiFi and ThingsBoard Configuration
 constexpr char WIFI_SSID[] = "Wokwi-GUEST";
 constexpr char WIFI_PASSWORD[] = "";
 constexpr char TOKEN[] = "7hVoTzIu2CfPV3hmM7m9";
@@ -18,13 +32,7 @@ constexpr uint16_t THINGSBOARD_PORT = 1883U;
 constexpr uint32_t MAX_MESSAGE_SIZE = 1024U;
 constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
 
-// Definiciones y variables
-constexpr int LED_PIN1 = 25;
-constexpr int LED_PIN2 = 33;
-constexpr int LED_PIN3 = 32;
-constexpr int LED_PIN4 = 26;
-constexpr int LED_PIN5 = 27;
-
+// Variables
 WiFiClient wifiClient;
 Arduino_MQTT_Client mqttClient(wifiClient);
 ThingsBoard tb(mqttClient, MAX_MESSAGE_SIZE);
@@ -43,7 +51,7 @@ constexpr std::array<const char *, 1U> CLIENT_ATTRIBUTES_LIST = {
   "deviceValue5"
 };
 
-// Inicialización de la conexión WiFi
+// Function to initialize WiFi
 void InitWiFi() {
   Serial.println("Connecting to AP ...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -54,7 +62,7 @@ void InitWiFi() {
   Serial.println("Connected to AP");
 }
 
-// Reintentar conexión WiFi
+// Function to reconnect WiFi
 bool reconnect() {
   if (WiFi.status() == WL_CONNECTED) {
     return true;
@@ -63,11 +71,11 @@ bool reconnect() {
   return true;
 }
 
-// Procesar cambios de switch
+// Callback function for processing switch changes
 RPC_Response processSwitchChange(const RPC_Data &data) {
   Serial.println(F("Received the set switch method"));
   deviceValue5 = data["switch"];
-  Serial.print(F("value change: "));
+  Serial.print(F("Value change: "));
   Serial.println(deviceValue5);
   attributesChanged = true;
 
@@ -76,16 +84,17 @@ RPC_Response processSwitchChange(const RPC_Data &data) {
   return RPC_Response(doc);
 }
 
+// Array of RPC callbacks
 const std::array<RPC_Callback, 1U> callbacks = {
   RPC_Callback{ "set_switch", processSwitchChange }
 };
 
-// Procesar atributos compartidos
+// Callback function for processing shared attributes
 void processSharedAttributes(const Shared_Attribute_Data &data) {
   for (auto it = data.begin(); it != data.end(); ++it) {
     const char* key = it->key().c_str();
     bool value = it->value().as<bool>();
-    
+
     if (strcmp(key, "deviceState1") == 0) {
       deviceState1 = value;
       digitalWrite(LED_PIN1, deviceState1);
@@ -115,7 +124,7 @@ void processSharedAttributes(const Shared_Attribute_Data &data) {
   }
 }
 
-// Procesar atributos del cliente
+// Callback function for processing client attributes
 void processClientAttributes(const Shared_Attribute_Data &data) {
   for (auto it = data.begin(); it != data.end(); ++it) {
     if (strcmp(it->key().c_str(), "deviceValue5") == 0) {
@@ -124,24 +133,30 @@ void processClientAttributes(const Shared_Attribute_Data &data) {
   }
 }
 
+// Define attribute callbacks
 const Shared_Attribute_Callback attributes_callback(&processSharedAttributes, SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend());
 const Attribute_Request_Callback attribute_shared_request_callback(&processSharedAttributes, SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend());
 const Attribute_Request_Callback attribute_client_request_callback(&processClientAttributes, CLIENT_ATTRIBUTES_LIST.cbegin(), CLIENT_ATTRIBUTES_LIST.cend());
 
 void setup() {
-  // Init
+  // Initialize Serial and LCD
+  Serial.begin(SERIAL_DEBUG_BAUD);
   lcd.init();
   lcd.backlight();
-  // Print something
   lcd.setCursor(0, 0);
   lcd.print("Air Conditioner");
 
-  Serial.begin(SERIAL_DEBUG_BAUD);
+  // Initialize DHT sensor
+  dht.begin();
+
+  // Initialize LED pins
   pinMode(LED_PIN1, OUTPUT);
   pinMode(LED_PIN2, OUTPUT);
   pinMode(LED_PIN3, OUTPUT);
   pinMode(LED_PIN4, OUTPUT);
   pinMode(LED_PIN5, OUTPUT);
+
+  // Initialize WiFi
   delay(1000);
   InitWiFi();
 }
@@ -185,13 +200,35 @@ void loop() {
 
   if (attributesChanged) {
     attributesChanged = false;
-    Serial.print("Saving attribute");
+    Serial.print("Saving attribute: ");
     Serial.println(deviceValue5);
     tb.sendAttributeData("deviceValue5", deviceValue5);
   }
 
   lcd.setCursor(0, 1);
   lcd.print(deviceValue5);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  char temperatureString[6];
+  dtostrf(t, 4, 1, temperatureString);
+
+  tb.sendTelemetryData("tempIndoor", temperatureString);
+  tb.sendTelemetryData("humidity", h);
+  tb.sendTelemetryData("deviceValue5", deviceValue5);
+  
+  tb.sendTelemetryData("tempOutdoor", random(10, 20));
+  tb.sendTelemetryData("energy", random(10, 20));
+
+  Serial.print("Temp ");
+  Serial.print(t);
+  Serial.println("°C");
+  Serial.print("Humidity ");
+  Serial.print(h);
+  Serial.println("%");
+  Serial.println("---");
+  delay(2000);
 
   tb.loop();
 }
